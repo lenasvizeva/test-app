1b842252c6885800059402647ec98a0f7d80286d
Helen <elijahsuzzivan@gmail.l.com>
2020-10-13 15:31:09 +0400

    Updates

---
+++ precache-manifest.125da88becd20308ee0c38bf74662d4b.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d97659f20e7f44e620e30ab0431d8a",
+    "url": "/lyingunderagreentree/test-app/index.html"
+  },
+  {
+    "revision": "15abd7e07de325f69084",
+    "url": "/lyingunderagreentree/test-app/static/css/2.3ee02f02.chunk.css"
+  },
+  {
+    "revision": "1593831016a99d9c676c",
+    "url": "/lyingunderagreentree/test-app/static/css/main.364b7df7.chunk.css"
+  },
+  {
+    "revision": "15abd7e07de325f69084",
+    "url": "/lyingunderagreentree/test-app/static/js/2.046c18bd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/lyingunderagreentree/test-app/static/js/2.046c18bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1593831016a99d9c676c",
+    "url": "/lyingunderagreentree/test-app/static/js/main.cbaa53ac.chunk.js"
+  },
+  {
+    "revision": "b51e4039d83b690bc434",
+    "url": "/lyingunderagreentree/test-app/static/js/runtime-main.c99ac612.js"
+  },
+  {
+    "revision": "6a1f7cfe6252b44b6ea1e3fbf5b6661b",
+    "url": "/lyingunderagreentree/test-app/static/media/NotoSans-Bold.6a1f7cfe.ttf"
+  },
+  {
+    "revision": "5131f12202c92cc280033f949a796fd2",
+    "url": "/lyingunderagreentree/test-app/static/media/NotoSans-ExtraLight.5131f122.ttf"
+  },
+  {
+    "revision": "21f6235482ecdf67217dfa18483f7716",
+    "url": "/lyingunderagreentree/test-app/static/media/NotoSans-Regular.21f62354.ttf"
+  },
+  {
+    "revision": "e2c2bc20049bf8fc82a94927ae111294",
+    "url": "/lyingunderagreentree/test-app/static/media/NotoSans-SemiBold.e2c2bc20.ttf"
+  },
+  {
+    "revision": "82338eee516bee325fb48103fe518d40",
+    "url": "/lyingunderagreentree/test-app/static/media/arrow_down_icon.82338eee.svg"
+  },
+  {
+    "revision": "3ad50374d563f8be1e91c52e36c6843d",
+    "url": "/lyingunderagreentree/test-app/static/media/burger_icon.3ad50374.svg"
+  },
+  {
+    "revision": "48f9bf5c5e0cf38b9266ea99240b5a34",
+    "url": "/lyingunderagreentree/test-app/static/media/call_icon.48f9bf5c.svg"
+  },
+  {
+    "revision": "e6bd2b92e0d3eac142a8b92d0cae2e5a",
+    "url": "/lyingunderagreentree/test-app/static/media/close_icon.e6bd2b92.svg"
+  },
+  {
+    "revision": "b575bda9e917364e9f2732c76296b1d9",
+    "url": "/lyingunderagreentree/test-app/static/media/gallery_pic.b575bda9.jpg"
+  },
+  {
+    "revision": "96a4caef383591c33614ebf3e3bf7cca",
+    "url": "/lyingunderagreentree/test-app/static/media/logo.96a4caef.svg"
+  },
+  {
+    "revision": "c83c6a016c5c782ca5f0f23bfea9b3c1",
+    "url": "/lyingunderagreentree/test-app/static/media/news00.c83c6a01.jpg"
+  },
+  {
+    "revision": "316523c37d2ee5a6af41d2b179e401ad",
+    "url": "/lyingunderagreentree/test-app/static/media/news01.316523c3.jpg"
+  },
+  {
+    "revision": "42504059dd5a30a4bbd3a72a07606b01",
+    "url": "/lyingunderagreentree/test-app/static/media/news02.42504059.jpg"
+  },
+  {
+    "revision": "a58236f35d5cef28e7cfbec081199c2c",
+    "url": "/lyingunderagreentree/test-app/static/media/news03.a58236f3.jpg"
+  },
+  {
+    "revision": "5bb4201b12886509cc3b212f2832a365",
+    "url": "/lyingunderagreentree/test-app/static/media/news04.5bb4201b.jpg"
+  },
+  {
+    "revision": "6a5226d1f5e8588739591a2975054ad3",
+    "url": "/lyingunderagreentree/test-app/static/media/news05.6a5226d1.jpg"
+  },
+  {
+    "revision": "2af2501e7add3d3bbf8d7e2168643281",
+    "url": "/lyingunderagreentree/test-app/static/media/news06.2af2501e.jpg"
+  },
+  {
+    "revision": "15364eebc2a5d45f9f00b32f15dfa893",
+    "url": "/lyingunderagreentree/test-app/static/media/news07.15364eeb.jpg"
+  },
+  {
+    "revision": "4e84eecc919fdfa11eb9ccd8ecadfe6c",
+    "url": "/lyingunderagreentree/test-app/static/media/news08.4e84eecc.jpg"
+  },
+  {
+    "revision": "ecf3cff3d4f0f662982957726f448bef",
+    "url": "/lyingunderagreentree/test-app/static/media/plus_icon.ecf3cff3.svg"
+  },
+  {
+    "revision": "dcabe40324f6d8252347e4f654de1787",
+    "url": "/lyingunderagreentree/test-app/static/media/quote_icon.dcabe403.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/lyingunderagreentree/test-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/lyingunderagreentree/test-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/lyingunderagreentree/test-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/lyingunderagreentree/test-app/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "10581d2964828e5d4dfc77f8283471df",
+    "url": "/lyingunderagreentree/test-app/static/media/slider_pic.10581d29.jpg"
+  }
+]);
